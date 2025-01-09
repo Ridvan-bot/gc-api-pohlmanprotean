57f46ba603c09dc7d9029fe8a59b70796dd0c67f
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-09 10:26:03 +0100

    fix: update deployment workflow and Dockerfile for Google Cloud Run integration

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,23 @@
-# Use Node.js as the base image
+# Use the official Node.js image as a base
 FROM node:20
 
 # Set the working directory
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# Copy package.json and package-lock.json
-COPY ["package*.json", "./"]
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
 
 # Install dependencies
-RUN npm install sharp
+RUN npm install
 
-# Copy the rest of the application code
+# Copy the rest of your application code to the working directory
 COPY . .
-
-# Expose the application port
-EXPOSE 8080
 
 # Build the TypeScript code
 RUN npm run build
 
+# Expose the port the app runs on
+EXPOSE 8080
+
 # Command to run the application
-CMD ["npm", "run","start"]
+CMD ["npm", "run", "start"]
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,33 @@
-// Import Request and Response correctly
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index';
 import { errorHandler } from './lib/errorHandling/errorHandler';
 
-const PORT = process.env.PORT || 8080;
-
 // Load environment variables
 dotenv.config();
 
-import logger from './lib/middleware/logger';
+const PORT = process.env.PORT || 8080;
 
-const app = express();  
-
+const app = express();
 
 // Use the logger middleware for all routes
+import logger from './lib/middleware/logger';
 app.use(logger);
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Register all routes with a common prefix
-app.use('/api/v1', routes); 
+app.use('/api/v1', routes);
 
 app.use((req, res, next) => {
- res.status(404).json({ message: 'Resource not found' });
- });
+  res.status(404).json({ message: 'Resource not found' });
+});
 
 // Error handling middleware
 app.use(errorHandler);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}/api/v1/welcome`);
+  console.log(`Server is running on http://localhost:${PORT}/api/v1/welcome`);
 });
