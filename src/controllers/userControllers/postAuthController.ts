33cb95b6bae5a7c2200e2fe10a6f63f45712b2ad
Robin Pohlman <robin.pohlman@gmail.com>
2025-01-10 13:11:09 +0100

    feat: integrate Google Cloud Secret Manager for JWT secret retrieval in authentication

--- src/controllers/userControllers/postAuthController.ts
+++ src/controllers/userControllers/postAuthController.ts
@@ -1,17 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcryptjs';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-dotenv.config();
 
-if (!process.env.JWT_SECRET) 
-  throw new Error('Required environment variables is missing');
+// Load Google cloud secrets
+
+const client = new SecretManagerServiceClient();
+
+const getSecrets = async (secretName: string, next: NextFunction) => {
+  try {
+    const [version] = await client.accessSecretVersion({
+      name: `projects/dev-gc-api-pohlmanprotean/secrets/${secretName}/versions/latest`,
+    });
+    const payload = version.payload?.data?.toString();
+    return payload;
+}
+catch (error) {
+  next(error);
+}
+}
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
 
   try {
-    console.log('username', username);
+    const jwtToken = await getSecrets('JWT_SECRET', next);
+    if (username && password && jwtToken) {
+      const token = jwt.sign({ username }, jwtToken, { 
+          expiresIn: '1h'
+        });
+        res.json({ message: 'Login successful', token });
+    }
+    else {
+      res.status(401).json({ message: 'Username or Password missing in request Unauthorized' });
+    }
   }
   catch (error) {
     next(error);
